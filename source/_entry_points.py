f0a98e593d0603101805d0dbe4c0e8fa111841fa
Shahar Azulay <shahar4@gmail.com>
2019-02-17 17:07:42 +0200

    working on entry points

--- source/_entry_points.py
+++ source/_entry_points.py
@@ -1,5 +1,12 @@
+import os
 import argparse
+import torch
+
 import _data_utils
+import _train
+
+cuda = torch.cuda.is_available()
+
 
 def init_datasets_main(args=None):
     parser = argparse.ArgumentParser(
@@ -11,6 +18,24 @@ def init_datasets_main(args=None):
     _data_utils.init_datasets(args.dir_path)
 
 
+def train_model(args=None):
+    parser = argparse.ArgumentParser(
+        description='Train the full model ans store on disk.')
+
+    _add_dir_path_to_parser(parser)
+    _add_batch_size_to_parser(parser)
+    _add_epochs_to_parser(parser)
+    args = parser.parse_args()
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
+
+    train_labeled_loader, train_unlabeled_loader, valid_loader = _data_utils.load_data(
+        data_path=args.dir_path, batch_size=args.batch_size, **kwargs)
+    Q, P = _train.train(train_labeled_loader, train_unlabeled_loader, valid_loader, epochs=args.n_epochs, n_classes=10, z_dim=2)
+    Q.save(os.path.join(args.dir_path, 'encoder'))
+    P.save(os.path.join(args.dir_path, 'decoder'))
+
+
 def _add_dir_path_to_parser(parser):
     parser.add_argument(
         '--dir-path',
@@ -18,6 +43,22 @@ def _add_dir_path_to_parser(parser):
         required=True,
         help='Path of the data directory')
 
+def _add_batch_size_to_parser(parser):
+    parser.add_argument(
+        '--batch-size',
+        dest='batch_size',
+        type=int,
+        default=100,
+        help='Batch size for the SGD during training process (default: 100)')
+
+def _add_epochs_to_parser(parser):
+    parser.add_argument(
+        '--n-epochs',
+        dest='n_epochs',
+        default=10,
+        type=int,
+        help='Number for training epochs (default: 10)')
 
 ### REMOVE LATER once setup.py is in place
-init_datasets_main()
+#init_datasets_main()
+train_model()
