4ef35d4afc57bd38ef4e6199e8b217c2da720841
Shahar Azulay <shaharazulay@Shahars-MacBook-Pro.local>
2019-03-19 14:35:11 +0200

    back to paper optimizers

--- source/_train_semi_supervised.py
+++ source/_train_semi_supervised.py
@@ -147,18 +147,18 @@ def _get_optimizers(models, config_dict, decay=1.0):
     classifier_lr = learning_rates['classifier_lr'] * decay
 
     # Set optimizators
-    auto_encoder_optim = optim.Adam(itertools.chain(Q.parameters(), P.parameters()), lr=auto_encoder_lr)
-
-    G_optim = optim.Adam(Q.parameters(), lr=generator_lr)
-    D_optim = optim.Adam(itertools.chain(D_gauss.parameters(), D_cat.parameters()), lr=discriminator_lr)
-
-    classifier_optim = optim.Adam(Q.parameters(), lr=classifier_lr)
+    # auto_encoder_optim = optim.Adam(itertools.chain(Q.parameters(), P.parameters()), lr=auto_encoder_lr)
+    #
+    # G_optim = optim.Adam(Q.parameters(), lr=generator_lr)
+    # D_optim = optim.Adam(itertools.chain(D_gauss.parameters(), D_cat.parameters()), lr=discriminator_lr)
+    #
+    # classifier_optim = optim.Adam(Q.parameters(), lr=classifier_lr)
 
     ###
-    # auto_encoder_optim = optim.SGD(itertools.chain(Q.parameters(), P.parameters()), lr=0.1 * decay, momentum=0.9)
-    # G_optim = optim.SGD(Q.parameters(), lr=0.1 * decay, momentum=0.1)
-    # D_optim = optim.SGD(itertools.chain(D_gauss.parameters(), D_cat.parameters()), lr=0.01 * decay, momentum=0.9)
-    # classifier_optim = optim.SGD(Q.parameters(), lr=0.1 * decay, momentum=0.9)
+    auto_encoder_optim = optim.SGD(itertools.chain(Q.parameters(), P.parameters()), lr=0.01 * decay, momentum=0.9)
+    G_optim = optim.SGD(Q.parameters(), lr=0.1 * decay, momentum=0.1)
+    D_optim = optim.SGD(itertools.chain(D_gauss.parameters(), D_cat.parameters()), lr=0.1 * decay, momentum=0.9)
+    classifier_optim = optim.SGD(Q.parameters(), lr=0.1 * decay, momentum=0.9)
     ###
     optimizers = auto_encoder_optim, G_optim, D_optim, classifier_optim
 
