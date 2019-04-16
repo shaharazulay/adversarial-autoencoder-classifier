38e4e1f653c0919b3e5db40e321909767887cd0b
shaharazulay <shahar4@gmail.com>
2019-04-16 15:56:02 +0300

    final fix in matplotlib

--- source/_visualization.py
+++ source/_visualization.py
@@ -6,8 +6,6 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 
 import matplotlib
-#matplotlib.use("TKAgg")
-
 from matplotlib import gridspec
 import matplotlib.pyplot as plt
 
