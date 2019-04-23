36d3c05b5efb7b79743f935ea755abc45866d865
shaharazulay <shahar4@gmail.com>
2019-04-23 13:35:02 +0300

    debug

--- source/_train_unsupervised.py
+++ source/_train_unsupervised.py
@@ -130,6 +130,7 @@ def _train_epoch(
         D_fake_gauss = D_gauss(z_fake_gauss)
 
         G_loss = - torch.mean(torch.log(D_fake_cat + epsilon)) - torch.mean(torch.log(D_fake_gauss + epsilon))
+        print(z_fake_gauss.norm())
         G_loss += z_fake_gauss.norm() * 10  # Z L2 regularization####
         
         G_loss.backward()
