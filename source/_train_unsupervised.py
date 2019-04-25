3e0e06551e35b4dd72181c2f67035ac25c5e7287
shaharazulay <shahar4@gmail.com>
2019-04-25 15:06:42 +0300

    using weights

--- source/_train_unsupervised.py
+++ source/_train_unsupervised.py
@@ -156,8 +156,9 @@ def _train_epoch(
         Q.eval()
         z_fake_cat, z_fake_gauss = Q(X)
 
-        #p_cat = get_adversarial_categorial_weights(z_fake_cat, batch_size, n_classes=n_classes)
         p_cat = None
+        if if params['use_disentanglement']:
+            p_cat = get_adversarial_categorial_weights(z_fake_cat, batch_size, n_classes=n_classes)
         
         z_real_cat = sample_categorical(batch_size, n_classes=n_classes, p=p_cat)
         z_real_gauss = Variable(torch.randn(batch_size, z_dim))
